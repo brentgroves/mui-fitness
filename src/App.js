8cf4c75ff6498144d49d044dd5d823422c18bf32
Brent Groves <brent.groves@gmail.com>
2019-11-06 20:43:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react'
-import { Paper, Typography, TextField, Button } from '@material-ui/core'
-//export default App;
-export default class App extends Component {  
-  state = { exercises: [],    title: ''  }
-  handleChange = ({ target: { name, value } }) =>  this.setState({[name]: value})
-//  render() {    return <h1>Exercises</h1>  }
-render() {    
-  const { title } = this.state    
-  return (     
-  <Paper> 
-    <Typography variant='h2' align='center' gutterBottom>
-    {title}
-    </Typography>      
-    <form>  
-      <TextField name='title' label='Exercise' value={title} onChange={this.handleChange} margin='normal' />
-<Button type='submit' color='primary' variant='outlined'>Create</Button>
-    </form>
-    </Paper>    
-    )  
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
+import NavBar from './navbar.js'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+
+          <Toolbar>
+            <TypoGraphy variant="h6"
+              color="inherit">
+              <NavBar />
+           </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+
+      </div>
+    );
   }
-  //render() {    return (<Typography variant='h1' align='center' gutterBottom>Exercises</Typography>)  }
 }
+export default App;
